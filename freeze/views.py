1cb8ef97c08ab3119596745484517d5e8cb13a6a
petersembi <petersembi@gmail.com>
2022-09-19 08:59:42 +0300

    modified:   freeze/views.py 	new file:   templates/freeze/home.html 	modified:   templates/freeze/index.html

--- freeze/views.py
+++ freeze/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
-    return render(request, 'freeze/index.html')
+    return render(request, 'freeze/index.html', {'price': 700})
