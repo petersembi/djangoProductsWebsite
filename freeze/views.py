d40b8a6a8bad81183b02b9cfff3aacf0a7b4edce
petersembi <petersembi@gmail.com>
2022-09-19 13:02:44 +0300

    new file:   assets/images/yoghurt_1.png 	new file:   assets/images/yoghurt_2.png 	new file:   assets/images/yoghurt_3.png 	new file:   freeze/migrations/0001_initial.py 	new file:   freeze/migrations/0002_destination_price.py 	modified:   freeze/models.py 	modified:   freeze/views.py 	new file:   static/images/yoghurt_1.png 	new file:   static/images/yoghurt_2.png 	new file:   static/images/yoghurt_3.png 	modified:   teluskoApp/settings.py 	modified:   templates/freeze/index.html

--- freeze/views.py
+++ freeze/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
-
+from .models import Destination
 
 # Create your views here.
 
 def index (request):
-    return render(request, 'freeze/index.html', {'price': 700})
+
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The sweetest youghurt'
+    dest1.price = '200'
+    dest1.img = 'yoghurt_1.png'
+    dest1.offer = False
+
+    dest2 = Destination()
+    dest2.name = 'Strawberry'
+    dest2.desc = 'The flavored youghurt'
+    dest2.price = '300'
+    dest2.img = 'yoghurt_2.png'
+    dest2.offer = True
+
+    dest3 = Destination()
+    dest3.name = 'Strawberry'
+    dest3.desc = 'The sweetest youghurt'
+    dest3.price = '200'
+    dest3.img = 'yoghurt_3.png'
+    dest3.offer = False
+
+    dests = [dest1, dest2, dest3]
+
+
+
+    # return render(request, 'freeze/index.html', {'price': 700})
+   
+    # passing each object individually
+    # return render(request, "freeze/index.html", {'dest1':dest1, 'dest2':dest2, 'dest3':dest3})
+   
+    # passing as only one object 
+    return render(request, "freeze/index.html", {'dests': dests})
--- teluskoApp/settings.py
+++ teluskoApp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'teluskoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'petsembi',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
+
     }
 }
 
